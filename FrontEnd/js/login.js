581087ac66f1c5b64b6c2b6be25456c7d83b15e3
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2023-06-01 17:14:33 +0200

    wip login js

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -4,3 +4,32 @@ submit.addEventListener("click", function (e) {
   e.preventDefault();
   FetchUserLogin();
 });
+
+async function FetchUserLogin() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  if (response.ok) {
+    const dataUser = await response.json();
+    localStorage.setItem("token", dataUser.token);
+    localStorage.setItem("login", true);
+    window.location.href = "./index.html";
+  } else {
+    localStorage.removeItem("token");
+    localStorage.removeItem("login");
+    document.querySelector(".errorMsg").innerText =
+      "Erreur : mauvais email ou mauvais mot de passe";
+  }
+}
